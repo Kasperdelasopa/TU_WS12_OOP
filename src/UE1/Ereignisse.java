d742948ce71639f0d374f7b0e22b77d17aad7125
Goran <goran@Gorans-MacBook-Air.local>
2012-10-23 04:01:34 +0200

    ereignisse changes

--- src/UE1/Ereignisse.java
+++ src/UE1/Ereignisse.java
@@ -6,109 +6,110 @@ import java.util.List;
 
 public class Ereignisse {
 
-	private ArrayList<Auftritt> auftritte;
+    private ArrayList<Auftritt> auftritte;
     private ArrayList<Probe> proben;
 
-	public Ereignisse() {
+    public Ereignisse() {
 
-            auftritte = new ArrayList<Auftritt>();
-            proben = new ArrayList<Probe>();
-	}
+    auftritte = new ArrayList<Auftritt>();
+    proben = new ArrayList<Probe>();
+    }
 
-	public List<Ereignis> getEreignisse() {
+    public List<Ereignis> getEreignisse() {
         
         List<Ereignis> tmp = new ArrayList<Ereignis>();
         tmp.addAll(auftritte);
         tmp.addAll(proben);
-		return tmp;
-	}
+	return tmp;
+    }
 
-	public List<Ereignis> getVergangene() {
 
-		List<Ereignis> tmp = new ArrayList<Ereignis>();
+    public List<Ereignis> getVergangene() {
 
-		for (Ereignis e : getEreignisse()) {
+	List<Ereignis> tmp = new ArrayList<Ereignis>();
 
-			if (e.getBestaetigt()) {
-				tmp.add(e);
-			}
+	for (Ereignis e : getEreignisse()) {
+
+		if (e.getBestaetigt()) {
+			tmp.add(e);
 		}
-
-		return tmp;
 	}
+
+	return tmp;
+    }
     
     public List<Ereignis> getZukuenftige() {
         
-		List<Ereignis> tmp = new ArrayList<Ereignis>();
+	List<Ereignis> tmp = new ArrayList<Ereignis>();
         
-		for (Ereignis e : getEreignisse()) {
+	for (Ereignis e : getEreignisse()) {
             
-			if (!e.getBestaetigt()) {
-				tmp.add(e);
-			}
-		}
-        
-		return tmp;
+            if (!e.getBestaetigt()) {
+		tmp.add(e);
+            }
 	}
+        
+	return tmp;
+    }
     
     public List<Auftritt> getAuftritte() {
         
-		return auftritte;
-	}
+	return auftritte;
+    }
     
     public List<Probe> getProben() {
         
-		return proben;
-	}
+	return proben;
+    }
     
     
     public List<Probe> getProben(Date von, Date bis) {
         
-		List<Probe> tmp = new ArrayList<Probe>();
+        List<Probe> tmp = new ArrayList<Probe>();
         
-		for (Probe e : getProben()) {
+	for (Probe e : getProben()) {
             
-			Date date = e.getDatum_zeit();
-			if ((von.before(date) && bis.after(date))
+		Date date = e.getDatum_zeit();
+		if ((von.before(date) && bis.after(date))
                 || (von.equals(date) || bis.equals(date))) {
-				tmp.add(e);
-			}
+			tmp.add(e);
 		}
-        
-		return tmp;
 	}
+        
+        return tmp;
+    }
     
     public List<Auftritt> getAuftritte(Date von, Date bis) {
         
-		List<Auftritt> tmp = new ArrayList<Auftritt>();
+        List<Auftritt> tmp = new ArrayList<Auftritt>();
         
-		for (Auftritt e : getAuftritte()) {
+	for (Auftritt e : getAuftritte()) {
             
-			Date date = e.getDatum_zeit();
-			if ((von.before(date) && bis.after(date))
+		Date date = e.getDatum_zeit();
+		if ((von.before(date) && bis.after(date))
                 || (von.equals(date) || bis.equals(date))) {
-				tmp.add(e);
-			}
+			tmp.add(e);
 		}
-        
-		return tmp;
 	}
+        
+	return tmp;
+    }
     
     public List<Ereignis> getEreignisse(Date von, Date bis) {
         
-		List<Ereignis> tmp = new ArrayList<Ereignis>();
+        List<Ereignis> tmp = new ArrayList<Ereignis>();
         
-		for (Ereignis e : getEreignisse()) {
+            for (Ereignis e : getEreignisse()) {
             
-			Date date = e.getDatum_zeit();
-			if ((von.before(date) && bis.after(date))
-                || (von.equals(date) || bis.equals(date))) {
-				tmp.add(e);
-			}
+		Date date = e.getDatum_zeit();
+		if ((von.before(date) && bis.after(date))
+                    || (von.equals(date) || bis.equals(date))) {
+			tmp.add(e);
 		}
-        
-		return tmp;
 	}
+        
+	return tmp;
+    }
     
     public void delProbe(int id) {
                 
@@ -119,27 +120,17 @@ public class Ereignisse {
                 
         auftritte.remove(searchAuftritt(id));
     }
-        
+   
+    
     public void delEreignis(int i) {
-        int pos=0;    
-            
-        for (Auftritt e : getAuftritte()) {
-            if(i==e.getId()){
-                auftritte.remove(pos);
-                return;}
-            
-                
-                pos++;
-        }
-        pos=0;
-        for (Probe e : getProben()) {
-            if(i==e.getId()){
-                proben.remove(pos);
-                return;}
-                pos++;
-        }
-        pos =-1;
-    }       
+       
+       
+        auftritte.remove(searchAuftritt(i));
+        proben.remove(searchProbe(i));
+        
+        
+    }
+     
         
     public int searchProbe(int i) {
         int pos=0;    
