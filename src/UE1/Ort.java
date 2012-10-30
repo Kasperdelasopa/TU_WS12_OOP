80c300f631b3925a709e16330926e27e29028221
Peter Nirschl <peter.nirschl@gmail.com>
2012-10-30 19:32:50 +0100

    Vereinbarungen zu Orte und Ort hinzugefügt

--- src/UE1/Ort.java
+++ src/UE1/Ort.java
@@ -11,12 +11,18 @@ public class Ort {
 	private String address;
 	private String country;
 	private String name;
+	// Invariante: infrastructure != null
 	private Collection<String> infrastructure;
 
+	// initialisiert Ort
 	public Ort() {
 		this.infrastructure = new ArrayList<String>();
 	}
 
+	// initialisiert Ort mit Standardparametern
+	// @param infrastructure != null
+	// FEHLER: die Bedingung infrastructure != null wird nicht auf Einhaltung
+	// geprüft
 	public Ort(String address, String country, String name,
 			Collection<String> infrastructure) {
 		this();
@@ -28,6 +34,7 @@ public class Ort {
 		this.infrastructure.addAll(infrastructure);
 	}
 
+	// liefert einen Hashcode für den Ort zurück
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -40,6 +47,8 @@ public class Ort {
 		return result;
 	}
 
+	// prüft den Ort auf Gleichheit mit dem Parameter obj und gibt true zurück
+	// wenn der Paremeter dem Objekt gleicht.
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -72,40 +81,51 @@ public class Ort {
 		return true;
 	}
 
+	// liefert eine String-Repräsentation des Objekts zurück
 	@Override
 	public String toString() {
 		return "Ort [address=" + address + ", country=" + country + ", name="
 				+ name + ", infrastructure=" + infrastructure + "]";
 	}
 
+	// liefert die Adresse des Orts zurück
 	public String getAddress() {
 		return address;
 	}
 
+	// setzt die Adresse des Ortes
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	// liefert das Land des Ortes zurück
 	public String getCountry() {
 		return country;
 	}
 
+	// setzt das Land des Ortes
 	public void setCountry(String country) {
 		this.country = country;
 	}
 
+	// liefert den Namen des Ortes zurück
 	public String getName() {
 		return name;
 	}
 
+	// setzt den Namen des Ortes
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	// liefert die verfügbare Inrastruktur des Ortes zurück
 	public Collection<String> getInfrastructure() {
 		return infrastructure;
 	}
 
+	// ersetzt die Infrastruktur des Ortes
+	// @param infrastructure != null
+	// FEHLER: die Invariante "infrastructure != NulL" wird nicht geprüft
 	public void setInfrastructure(Collection<String> infrastructure) {
 		this.infrastructure = infrastructure;
 	}
--- src/UE1/Orte.java
+++ src/UE1/Orte.java
@@ -5,16 +5,21 @@ import java.util.Collection;
 
 public class Orte {
 
+	// Invariante: orte != null
 	private Collection<Ort> orte;
 
+	// initialisiert die Orte-Sammlung
 	public Orte() {
 		orte = new ArrayList<Ort>();
 	}
 
+	// liefert die gesammelten Orte zurück
 	public Collection<Ort> getOrte() {
 		return orte;
 	}
 
+	// liefert alle Orte zurück, an denen das Infrastrukturelement
+	// "infrastructure" verfügbar ist.
 	public Collection<Ort> findOrteByInfrastructure(String infrastructure) {
 		Collection<Ort> result = new ArrayList<Ort>();
 
