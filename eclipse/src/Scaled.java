f3b3a0c11c79df17e032dd9428fe46473df6c844
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-10 11:41:26 +0100

    UE4: finished implementation of Scaled

--- eclipse/src/Scaled.java
+++ eclipse/src/Scaled.java
@@ -1,20 +1,26 @@
 public class Scaled<P extends Pict> extends AbstractCompositePict<P> {
-
+	// instances represent pict compositions, that are scaled with
+	// their own scale-implementation.
+	
+	private final static String LINE_FEED = System.getProperty("line.separator");
+	// constant holding the line feed for performance reasons
+	
 	public Scaled(P[][] inhalt) {
 		this.inhalt = inhalt;
 	}
+	// inhalt != null; 
+	// creates a new instance of Scaled and initializes the content array
 
 	@Override
 	public String toString() {
 		StringBuilder stringRep = new StringBuilder();
-		
+				
 		for(P[] row : inhalt) {
 			for(P column : row) {
 				stringRep.append(column.toString());
 			}
-			
+			stringRep.append(LINE_FEED);
 		}
-		
 		return stringRep.toString();
 	}
 
