f6bffb9e1c8a6f9fc62f5dadf669bfc36939a1b7
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-10 11:43:31 +0100

    UE4: moved LINE_FEED constant to AbstractCompositePict class

--- eclipse/src/AbstractCompositePict.java
+++ eclipse/src/AbstractCompositePict.java
@@ -3,6 +3,9 @@ public abstract class AbstractCompositePict<P> implements Pict {
 	// instances represent compositions of 2-dimensional pictures
 	// consists of objects of type P that have a string representation
 	
+	protected final static String LINE_FEED = System.getProperty("line.separator");
+	// constant holding the line feed for performance reasons
+	
 	protected P[][] inhalt;
 	// inhalt != null; objects that form the picture composition
 	
