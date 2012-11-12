e9013cc0f62c86af0577714261b583b59d6a38b1
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-12 21:01:43 +0100

    UE4: add test cases for Scaled in Test.java

--- eclipse/src/Test.java
+++ eclipse/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
+		// NOTE Tests for Box start here
 		System.out.println("*********************************************************");
 		System.out.println("Box: hoehe = 2.1; breite = 3.4; rand = 'o'; inhalt = '.' -> muss aufgerundet werden:");
 		Box b = new Box(2.1, 3.4, 'o', '.' );	
@@ -37,6 +38,7 @@ public class Test {
 		System.out.println(b.toString());
 		System.out.println("*********************************************************");
 		
+		// NOTE tests for ClearBox start here
 		System.out.println("*********************************************************");
 		System.out.println("ClearBox: hoehe = 4; breite = 4; rand = '*'; inhalt = ' '");
 		ClearBox cb = new ClearBox(4.0, 4.0);	
@@ -50,6 +52,43 @@ public class Test {
 		System.out.println(cb.toString());
 		System.out.println("Seitenverhätnis: "+cb.getSeitenverhältniss().toString());
 		System.out.println("*********************************************************");
+		
+		// NOTE: Tests for FreeBox start here
+		
+		
+		// NOTE: Tests for DarkBox start here
+		
+		
+		// NOTE: Tests for Scaled start here
+		Box scaledBox1 = new Box(2.0, 2.0, 'o', '.' );
+		Box scaledBox2 = new Box(2.0, 2.0, 'o', '.' );
+		Box[][] scaledArray1 = { {scaledBox1}, {scaledBox2} };
+		Scaled<Box> scaled1 = new Scaled<Box>(scaledArray1);
+		
+		ClearBox scClearBox = new ClearBox(3.0, 2.0);
+		ClearBox[][] scClearBoxArray = { {scClearBox} };
+		Scaled<ClearBox> scaled2 = new Scaled<ClearBox> (scClearBoxArray);
+		
+		System.out.println("*********************************************************");
+		System.out.println("Scaled: [Box  hoehe = 2; breite = 2; rand = 'o'; inhalt = '.'], [Box hoehe = 2; breite = 2; rand = 'o'; inhalt = '.']");
+		System.out.println(scaled1.toString());
+		System.out.println("*********************************************************");
+
+		System.out.println("*********************************************************");
+		System.out.println("Scaled: [Box  hoehe = 2; breite = 2; rand = 'o'; inhalt = '.'], [Box hoehe = 2; breite = 2; rand = 'o'; inhalt = '.']");
+		System.out.println("scale(2.3)");
+		scaled1.scale(2.3);
+		System.out.println(scaled1.toString());
+		System.out.println("*********************************************************");
+
+		System.out.println("*********************************************************");
+		System.out.println("Scaled: [ClearBox  hoehe = 3; breite = 2]");
+		System.out.println("scale(3.0)");
+		scaled2.scale(3.0);
+		System.out.println(scaled2.toString());
+		System.out.println("*********************************************************");
+		
+		// NOTE Test for type Repeated start here
 		
 	}
 }
