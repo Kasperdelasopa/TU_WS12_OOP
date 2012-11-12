11cad6003a4a1f02b1a6f9300ede3686b5524aa0
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-12 21:12:54 +0100

    UE4: implement scale() method in Repeated; add constraints to memebrs of Repeated

--- eclipse/src/Repeated.java
+++ eclipse/src/Repeated.java
@@ -1,22 +1,27 @@
-
 public class Repeated<P> extends AbstractCompositePict<P> {
-
+	// instances represent pict compositions, that are scaled with
+	// a scale factor
+	
 	private double skalierungsfaktor;
 	
 	public Repeated(P[][] inhalt) {
 		this.inhalt = inhalt;
+		this.skalierungsfaktor = 1.0;
 	}
+	// inhalt != null; 
+	// creates a new instance of Repeated and initializes the content array
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder();
+
+		
+		return sb.toString();
 	}
 
 	@Override
 	public void scale(double factor) {
-		// TODO Auto-generated method stub
-		
+		this.skalierungsfaktor *= factor;
 	}
 
 }
