cfdc529e277d683e7811bd64ad04134a2ec917e5
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-13 19:05:22 +0100

    UE4: add further constraints to Scaled and Repeated

--- eclipse/src/Repeated.java
+++ eclipse/src/Repeated.java
@@ -3,6 +3,7 @@ public class Repeated<P> extends AbstractCompositePict<P> {
 	// a scale factor
 	
 	private double skalierungsfaktor;
+	// skalierungsfaktor > 0.0
 	
 	public Repeated(P[][] inhalt) {
 		this.inhalt = inhalt;
@@ -18,10 +19,12 @@ public class Repeated<P> extends AbstractCompositePict<P> {
 		
 		return sb.toString();
 	}
+	// returns the picture as String
 
 	@Override
 	public void scale(double factor) {
 		this.skalierungsfaktor *= factor;
 	}
+	// 0.1 <= factor <= 10.0; resize the picture composition
 
 }
