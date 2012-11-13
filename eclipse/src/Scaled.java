c3f5e528723f7e9aa088fc3a2190dd865e71338e
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-13 20:02:44 +0100

    UE4: moved toString() in Repeated and Scaled to upper class AbstractCompositePict

--- eclipse/src/Scaled.java
+++ eclipse/src/Scaled.java
@@ -3,25 +3,11 @@ public class Scaled<P extends Pict> extends AbstractCompositePict<P> {
 	// their own scale-implementation.
 	
 	public Scaled(P[][] inhalt) {
-		this.inhalt = inhalt;
+		super(inhalt);
 	}
 	// inhalt != null; 
 	// creates a new instance of Scaled and initializes the content array
 
-	@Override
-	public String toString() {
-		StringBuilder stringRep = new StringBuilder();
-				
-		for(P[] row : inhalt) {
-			for(P column : row) {
-				stringRep.append(column.toString());
-			}
-			stringRep.append(LINE_FEED);
-		}
-		return stringRep.toString();
-	}
-	// returns the picture as String
-	
 	@Override
 	public void scale(double factor) {
 		for (P[] row : inhalt) {
