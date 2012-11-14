0f541abb69d030351fb0472ef5f62b61fd4723d5
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-14 21:58:23 +0100

    UE5: implement OrderedSet functionality

--- eclipse/src/OrderedSet.java
+++ eclipse/src/OrderedSet.java
@@ -3,7 +3,17 @@ public class OrderedSet<P extends Shorter<P>> extends Set<P> {
 	
 	@Override
 	public void insert(P element) {
-		// TODO implement
+		SetElement current = startElement;
+		
+		if(current == null) {
+			startElement = new SetElement(element, null);
+		} else {
+			while(current.getValue().shorter(element) && current.getNext() != null) {
+				current = current.getNext();
+			}
+			SetElement successor = current.getNext();
+			current.setNext(new SetElement(element, successor));
+		}
 	}
 	
 }
