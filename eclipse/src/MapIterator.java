ebe0513c5b5d65c8800243f2ae620d7a4435618d
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-17 21:29:53 +0100

    UE5: redesign of MapIterator as interface

--- eclipse/src/MapIterator.java
+++ eclipse/src/MapIterator.java
@@ -1,16 +1,17 @@
 import java.util.Iterator;
 
 
-public class MapIterator<P ,Q> extends SetIterator<P> {
+public interface MapIterator<P ,Q> extends Iterator<P> {
+	// implementing types enable access to a sequence of P-type elements,
+	// adding elements to this sequence and getting an iterator for accessing
+	// the Q-type sequence, that is referenced by a single P-type entry.
 	
-	public MapIterator(Set<P>.SetElement e) {
-		super(e);
-		// TODO Auto-generated constructor stub
-	}
-
-
-	public Iterator<Q> iterator(){
-
-		return null;
-	}
+	public void add(P element);
+	// Inserts the specified element into the list. The element is inserted 
+	// immediately before the next element that would be returned by next, 
+	// if any.
+	
+	public Iterator<Q> iterator();
+	// returns an iterator for the sequence of Q-type elements, that are 
+	// referenced by the current P-type element
 }
