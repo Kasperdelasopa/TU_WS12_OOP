97d9c8dcb3df86f5fe2b1f6051a7ffd296a92d7d
Manuel Schmitt <manuel.t.schmitt@gmail.com>
2012-11-21 19:57:21 +0100

    insert Zusicherungen create find method in Tracker

--- eclipse/src/SensorAktorenKit_10kW.java
+++ eclipse/src/SensorAktorenKit_10kW.java
@@ -5,29 +5,38 @@ public class SensorAktorenKit_10kW extends SensorAktorenKit {
 	public SensorAktorenKit setSensorAktorenKitForAndroid(Bediener bediener) {
 		return null;
 	}
+	//a Bediener is not allowed to use a 10KW SensorAktorKit
+	//null will be returned instead of an error message 
 
 	@Override
 	public SensorAktorenKit setSensorAktorenKitForAndroid(Kaempfer kaempfer) {
 		return this;
 	}
+	//a Kaempfer is allowed to use a 10KW SensorAktorKit
+	//so this object will be returned; 
 
 	@Override
 	public SensorAktorenKit setSensorAktorenKitForAndroid(Android android,
 			Software_Stufe3 software) {
 		return null;
 	}
+	//a android with Software-Level 3 is not allowed to use a 10KW SensorAktorKit
+	//null will be returned instead of an error message 
 
 	@Override
 	public SensorAktorenKit setSensorAktorenKitForAndroid(Android android,
 			Software_Stufe4 software) {
 		return this;
 	}
+	//a android with a Software-Level 4 is allowed to use a 10KW SensorAktorKit
+	//so this object will be returned; 
 
 	@Override
 	public SensorAktorenKit setSensorAktorenKitForAndroid(Android android,
 			Software software) {
 		return null;
 	}
+	//for any other android null will be returned; 
 
 	@Override
 	public String toString(){
