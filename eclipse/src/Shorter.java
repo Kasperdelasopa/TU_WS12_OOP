1a8305487b5409ba215ca11c5777338ae5980fde
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-21 12:20:43 +0100

    UE6: repository cleanup

--- eclipse/src/Shorter.java
+++
@@ -1,9 +0,0 @@
-
-public interface Shorter<P> {
-	// implementing subtypes are somehow comparable by their length/shortness.
-	// P must be the type of the implementing subtype.
-	
-	public boolean shorter(P element);
-	// @param element != null;
-	// returns true if the current instance is shorter than element.
-}
