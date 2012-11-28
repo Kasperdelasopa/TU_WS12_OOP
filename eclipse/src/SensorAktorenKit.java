c14052def5bd3a07b85b79eeef4673defafc54cf
Peter Nirschl <peter.nirschl@gmail.com>
2012-11-28 13:11:54 +0100

    cleanup for UE7

--- eclipse/src/SensorAktorenKit.java
+++
@@ -1,6 +0,0 @@
-
-public abstract class SensorAktorenKit extends AbstractAndroidPart {
-	public abstract SensorAktorenKit setSensorAktorenKitForAndroid(Bediener bediener);
-	public abstract SensorAktorenKit setSensorAktorenKitForAndroid(Kaempfer kaempfer);
-	public abstract SensorAktorenKit setSensorAktorenKitForAndroid(Android android);
-}
--- eclipse/src/Software_Hilfskraft_Stufe2.java
+++
@@ -1,9 +0,0 @@
-
-public class Software_Hilfskraft_Stufe2 extends Software_Hilfskraft_Stufe1 {
-
-	@Override
-	public String toString(){
-		return "Hilfskraft-Software d. Stufe 2";
-	}
-	//returns the description for this class
-}
