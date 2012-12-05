1c42270a444886830d24657f2afae0dd54be400c
Manuel Schmitt <manuel.t.schmitt@gmail.com>
2012-12-05 22:04:01 +0100

    added Traktor, DieselTaktor, GasTraktor, IFunktion, Drillmaschiene, Duengestreuer impementation + assertions + annotations

--- eclipse/src/GasTraktor.java
+++ eclipse/src/GasTraktor.java
@@ -1,10 +1,26 @@
 @Guarantor(person="Manuel Schmitt")
 public class GasTraktor extends Traktor {
  
+	private double verbrauchteMenge = 0.0;
+	
 	public GasTraktor(){}
 	
 	@Guarantor(person="Manuel Schmitt")
 	public String toString(){
 		return "Gastraktor";
 	}
+	//returns a description
+
+	@Guarantor(person="Manuel Schmitt")
+	public double getVerbrauchteMenge() {
+		return verbrauchteMenge;
+	}
+	// returns the used gas
+
+	@Guarantor(person="Manuel Schmitt")
+	public void incrementVerbrauch() {
+		this.verbrauchteMenge += 1;
+	}
+	// sets the used gas
+	// QverbrauchteMenge must be > 0
 }
--- eclipse/src/IFunktion.java
+++ eclipse/src/IFunktion.java
@@ -1,4 +1,5 @@
 
 public interface IFunktion {
-
+	public Integer getMengeSaeschare();
+	public Double getFassungskapazitaet();
 }
