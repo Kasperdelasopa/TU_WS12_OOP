9bb55b7dfb4c3ac2ffbe8f2959c87022ee4399d2
Peter Nirschl <peter.nirschl@gmail.com>
2012-12-11 19:37:25 +0100

    resolved merge conflict

--- eclipse/src/IFunktion.java
+++ eclipse/src/IFunktion.java
@@ -1,16 +1,11 @@
 
 @Guarantor(person="Manuel Schmitt")
 public interface IFunktion {
-<<<<<<< HEAD
-	@Guarantor(person="Manuel Schmitt")
-	public Integer getMengeSaeschare();
-	
-=======
 	//This class provides a function to a Traktor
 	
 	@Guarantor(person="Manuel Schmitt")
 	public Integer getMengeSaeschare();
->>>>>>> ms
+
 	@Guarantor(person="Manuel Schmitt")
 	public Double getFassungskapazitaet();
 }
