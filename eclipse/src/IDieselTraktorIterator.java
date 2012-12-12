a8e02656074cd179d92a5236fc4479a711f1d410
Peter Nirschl <peter.nirschl@gmail.com>
2012-12-12 16:40:56 +0100

    kickoff for Uebung 9

--- eclipse/src/IDieselTraktorIterator.java
+++
@@ -1,9 +0,0 @@
-
-@Guarantor(person="Peter Nirschl")
-public interface IDieselTraktorIterator extends IBaseIterator {
-	// An iterator over a collection holding DieselTraktor items.
-	
-	@Guarantor(person="Peter Nirschl")
-	public DieselTraktor next();
-	// Returns the next element in the iteration.
-}
