69d41736c9d468e81a5fcda1e4042e9aa92a0859
Goran <goran@Gorans-MacBook-Air.local>
2012-12-16 17:39:20 +0100

    implementation of KeksBackmaschine, DoppelkeksBackmaschine

--- eclipse/src/KeksBackmaschine.java
+++ eclipse/src/KeksBackmaschine.java
@@ -16,7 +16,17 @@ public class KeksBackmaschine {
 	public List<Crunchable> createKekse(Bestellung bestellung){
 		List<Crunchable> list = new ArrayList<Crunchable>(); 
 		
-		//TODO..
+		if(bestellung.getFuellung==null){
+			for(int x=0; x<bestellung.getAnzahl(); x++){
+				list.add(new Keks(bestellung.getTeigart(), this.form));
+			}
+		}
+		else{
+			Keks prototyp = new Keks(bestellung.getTeigart(), this.form);
+			for(int x=0; x<bestellung.getAnzahl(); x++){
+				list.add(DoppelkeksBackmaschine.getInstance().createKeks(prototyp, bestellung.getFuellung()));
+			}
+		} 
 		
 		return list;
 	}
