ee15d23ff9c20633ddbd45161a69353dfbf83699
Goran <goran@Gorans-MacBook-Air.local>
2012-12-17 20:00:11 +0100

    Test.java

--- eclipse/src/Test.java
+++ eclipse/src/Test.java
@@ -1,7 +1,12 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Test {
 
 	public static void main(String[] args) {
+		
+		/*****Bestellungen****************************************************************/
 		Bestellung bestellung = new Bestellung();
 		
 		bestellung.addPosition(new BestellPosition(1, Teigart.MUERBTEIG, 
@@ -20,5 +25,25 @@ public class Test {
 
 		bestellung.drucken();
 
+
+		/*****Baeckerei**Fuer jede Form eine Backmaschine****************************************************************/
+		HashMap<Form, KeksBackmaschine> map = new HashMap<Form, KeksBackmaschine>();
+		
+		for (Form form : Form.values()) {
+			map.put(form, new KeksBackmaschine(form));
+		}			
+		
+		/*****Kekse backen************************************************************************************************/
+		List<Crunchable> keksliste = new ArrayList<Crunchable>();
+		
+		for(BestellPosition p : bestellung.getPositions()){
+			KeksBackmaschine x = map.get(p.getForm());
+			keksliste.addAll(x.createKekse(p));
+		}
+		
+		/*****Kekse in Keksdose*******************************************************************************************/
+		System.out.println("KEKSDOSE: ");
+		Keksdose dose = new Keksdose(keksliste);
+		dose.inhalt();
 	}
 }
